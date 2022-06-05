d6f603e398c1fa76ab990ae55765751d5e85e124
shimizu-saffle <shimizu.saffle@gmail.com>
2022-06-05 21:00:46 +0900

    FormError

--- lib/screens/sign_in/components/body.dart
+++ lib/screens/sign_in/components/body.dart
@@ -1,7 +1,9 @@
+import 'package:e_commerce_app_ui_sample/components/default_button.dart';
 import 'package:e_commerce_app_ui_sample/size_config.dart';
 import 'package:flutter/material.dart';
 
 import 'custom_suffix_icon.dart';
+import 'form_error.dart';
 
 class Body extends StatelessWidget {
   const Body({Key? key}) : super(key: key);
@@ -46,19 +48,54 @@ class SignForm extends StatefulWidget {
 }
 
 class _SignFormState extends State<SignForm> {
+  final _formKey = GlobalKey<FormState>();
+  final List<String> errors = ['Demo Error'];
   @override
   Widget build(BuildContext context) {
     return Form(
         child: Column(
       children: [
-        TextFormField(
-          decoration: const InputDecoration(
-            labelText: 'Email',
-            hintText: 'Enter your email',
-            suffixIcon: CustomSuffixIcon(svgIcon: 'assets/icons/Mail.svg'),
-          ),
-        ),
+        buildEmailFormField(),
+        SizedBox(height: getProportionateScreenHeight(20)),
+        buildPasswdFormField(),
+        SizedBox(height: getProportionateScreenHeight(20)),
+        FormError(errors: errors),
+        DefaultButton(
+          text: 'Continue',
+          onPressed: () {},
+        )
       ],
     ));
+  }
+
+  TextFormField buildPasswdFormField() {
+    return TextFormField(
+      keyboardType: TextInputType.visiblePassword,
+      obscureText: true,
+      decoration: const InputDecoration(
+        labelText: 'Password',
+        hintText: 'Enter your password',
+        suffixIcon: CustomSuffixIcon(svgIcon: 'assets/icons/Lock.svg'),
+      ),
+    );
+  }
+
+  TextFormField buildEmailFormField() {
+    return TextFormField(
+      keyboardType: TextInputType.emailAddress,
+      validator: (value) {
+        if (value!.isEmpty) {
+          setState(() {
+            errors.add('Please enter your email');
+          });
+        }
+        return;
+      },
+      decoration: const InputDecoration(
+        labelText: 'Email',
+        hintText: 'Enter your email',
+        suffixIcon: CustomSuffixIcon(svgIcon: 'assets/icons/Mail.svg'),
+      ),
+    );
   }
 }
