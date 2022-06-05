57d2cf1755aca0c667177eeac14e4704d18b9fe7
shimizu-saffle <shimizu.saffle@gmail.com>
2022-06-05 17:57:58 +0900

    SignInScreen.routeName

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,6 +1,8 @@
+import 'package:e_commerce_app_ui_sample/screens/sign_in/sign_in_screen.dart';
 import 'package:e_commerce_app_ui_sample/screens/splash/splash_screen.dart';
 import 'package:flutter/widgets.dart';
 
 final Map<String, WidgetBuilder> routes = {
   SplashScreen.routeName: (context) => const SplashScreen(),
+  SignInScreen.routeName: (context) => const SignInScreen(),
 };
---
+++ lib/screens/sign_in/sign_in_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class SignInScreen extends StatelessWidget {
+  const SignInScreen({Key? key}) : super(key: key);
+  static String routeName = '/sign_in';
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
